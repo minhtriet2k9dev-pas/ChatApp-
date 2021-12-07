290f7ecb1bb735fe2eb40f953524df8b0a4c0543
minhtriet2k9 <minhtrietamoled@gmail.com>
2021-12-07 12:00:50 +0700

    ChatApp-

--- index.js
+++ index.js
@@ -11,18 +11,34 @@ const fs = require('fs');
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
-fs.writeFile(__dirname + '/chat.log',"",function (err) {
-    if (err){
-        console.log(err);
-    }
-})
+if(!fs.existsSync('/chat.log')){
+    fs.writeFile(__dirname + '/chat.log',"",function (err) {
+        if (err){
+            console.log(err);
+        }
+    })
+}
+// function getChatLog(){
+//     fs.readFile(__dirname + '/chat.log','utf-8', function(err,data){
+//         if (!err) {
+//             return data;
+//         } else {
+//             console.log(err);
+//         }
+//     });
+// }
+
+// var chatLog = getChatLog();
+// console.log(chatLog);
+// io.emit('chat-log',chatLog);
+
 io.on('connection',(socket) => {
     var time = new Date();
-    socket.on('new-user',(name) => {
+    socket.on('new-user',(data) => {
         //console.log('User connected at',time.getHours()+':'+time.getMinutes()+':'+time.getSeconds(),`with name: "${name}"`);
-        fs.appendFile(__dirname+"/chat.log",'User connected at '+time.getHours()+':'+time.getMinutes()+':'+time.getSeconds()+` with name: "${name}"\n`, function(err) {
+        fs.appendFile(__dirname+"/chat.log",'User connected at '+time.getHours()+':'+time.getMinutes()+':'+time.getSeconds()+` with name: "${data.name}" , ip "${data.ip}"\n`,  function(err) {
             if(err) {
-                return console.log(err);
+                console.log(err);
             }
         }); 
     });
